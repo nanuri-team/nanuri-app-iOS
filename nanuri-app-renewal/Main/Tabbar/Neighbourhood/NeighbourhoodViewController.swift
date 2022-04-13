44df44ed078aa822249aa987bcd26e9b4b0addc1
min <minimani0421@gmail.com>
2022-04-14 04:32:01 +0900

    ADD: 내 주변 상품 뷰 완료

--- nanuri-app-renewal/Main/Tabbar/Neighbourhood/NeighbourhoodViewController.swift
+++ nanuri-app-renewal/Main/Tabbar/Neighbourhood/NeighbourhoodViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class NeighbourhoodViewController: UIViewController {
+    
+    var collectionCellWidth = 120
+    var collectionCellHeight = 163
+    let ratioWidth = UIScreen.main.bounds.width
+    var filterButtonArray: [FilterButton] = []
+    var filterCount = 6 + 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +31,20 @@ class NeighbourhoodViewController: UIViewController {
         self.navigationItem.setLeftBarButton(location, animated: true)
         self.navigationItem.setRightBarButton(notificationButton, animated: true)
         
+//        setUpNoSettingView()
+        
+        
+        
+        // filter button
+        for i in 0..<filterCount {
+            let filterButton = FilterButton(filterType: i)
+            filterButton.tag = i
+            filterButton.isSelected = false
+            filterButton.addTarget(self, action: #selector(selectFilterButton), for: .touchUpInside)
+            filterButtonArray.append(filterButton)
+        }
+        
+        
         setUpView()
     }
     
@@ -39,7 +59,16 @@ class NeighbourhoodViewController: UIViewController {
         print("noti")
     }
     
-    func setUpView() {
+    @objc func selectFilterButton(sender: UIButton) {
+        print(sender.tag)
+        for i in 0..<filterButtonArray.count {
+            filterButtonArray[i].isSelected = false
+        }
+        
+        filterButtonArray[sender.tag].isSelected = true
+    }
+    
+    func setUpNoSettingView() {
         let noSettingLocationView = UIView()
         self.view.addSubview(noSettingLocationView)
         
@@ -78,5 +107,164 @@ class NeighbourhoodViewController: UIViewController {
             make.center.equalToSuperview()
         }
     }
+    
+    func setUpView() {
+        
+        let headerView = UIView()
+        let headerViewHeight = collectionCellHeight + 30 + 40 + 30
+        headerView.frame = CGRect(x: 0, y: 0, width: Int(ratioWidth), height: headerViewHeight)
+        
+        let hotItemTitleLabel = UILabel()
+        hotItemTitleLabel.attributedText = .attributeFont(font: .PBold, size: 17, text: "내 주변 인기 상품", lineHeight: 20.4)
+        headerView.addSubview(hotItemTitleLabel)
+        hotItemTitleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(24)
+            make.right.left.equalToSuperview().inset(16)
+        }
+        
+        let layout = UICollectionViewFlowLayout()
+        let neighbourhoodCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        neighbourhoodCollectionView.delegate = self
+        neighbourhoodCollectionView.dataSource = self
+        neighbourhoodCollectionView.register(VerticalProductCollectionViewCell.self, forCellWithReuseIdentifier: VerticalProductCollectionViewCell.cellId)
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 16
+        headerView.addSubview(neighbourhoodCollectionView)
+        neighbourhoodCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(hotItemTitleLabel.snp.bottom).inset(-10)
+            make.width.equalTo(ratioWidth)
+            make.height.equalTo(collectionCellHeight)
+        }
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .nanuriGray1
+        headerView.addSubview(separatorView)
+        separatorView.snp.makeConstraints { make in
+            make.top.equalTo(neighbourhoodCollectionView.snp.bottom).inset(-24)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(16)
+        }
+        
+        let neighbourhoodTableView = UITableView()
+        neighbourhoodTableView.delegate = self
+        neighbourhoodTableView.dataSource = self
+        neighbourhoodTableView.separatorInset = .zero
+        neighbourhoodTableView.separatorStyle = .none
+        neighbourhoodTableView.tableHeaderView = headerView
+        neighbourhoodTableView.register(MainProductTableViewCell.self, forCellReuseIdentifier: MainProductTableViewCell.cellId)
+        self.view.addSubview(neighbourhoodTableView)
+        neighbourhoodTableView.snp.makeConstraints { make in
+            make.top.left.right.bottom.equalToSuperview()
+        }
+        
+    }
 
+}
+
+
+//MARK: - UITableViewDelegate, UITableViewDataSource
+
+extension NeighbourhoodViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView = UIView()
+        headerView.backgroundColor = .white
+        headerView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 35)
+        
+        let filterScrollView = UIScrollView()
+        filterScrollView.contentSize = CGSize(width: filterScrollView.frame.width, height: filterScrollView.frame.height)
+        filterScrollView.showsHorizontalScrollIndicator = false
+        filterScrollView.clipsToBounds = true
+        headerView.addSubview(filterScrollView)
+        filterScrollView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(5)
+            make.width.equalToSuperview()
+            make.height.equalTo(35)
+        }
+        
+        for i in 0..<filterButtonArray.count {
+            filterScrollView.addSubview(filterButtonArray[i])
+            if i != 0 {
+                // 마지막 버튼일 때
+                if i == filterButtonArray.count - 1 {
+                    filterButtonArray[i].snp.makeConstraints { make in
+                        make.left.equalTo(filterButtonArray[i - 1].snp.right).inset(-6)
+                        make.right.equalToSuperview()
+                    }
+                } else { // 나머지 버튼
+                    filterButtonArray[i].snp.makeConstraints { make in
+                        make.left.equalTo(filterButtonArray[i - 1].snp.right).inset(-6)
+                    }
+                }
+            } else { // 첫번째 버튼일 때
+                filterButtonArray[i].snp.makeConstraints { make in
+                    make.left.equalToSuperview().inset(16)
+                }
+            }
+        }
+       
+        
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "\(indexPath.row)"
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: identifier) {
+            return reuseCell
+        } else {
+            let cell = MainProductTableViewCell.init(style: .default, reuseIdentifier: identifier)
+            cell.selectionStyle = .none
+            
+            return cell
+        }
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let productDetailViewController = ProductDetailViewController()
+        productDetailViewController.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(productDetailViewController, animated: true)
+    }
+}
+
+extension NeighbourhoodViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionCellWidth, height: collectionCellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: VerticalProductCollectionViewCell.cellId, for: indexPath) as? VerticalProductCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let productDetailViewController = ProductDetailViewController()
+        productDetailViewController.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(productDetailViewController, animated: true)
+    }
+    
 }
