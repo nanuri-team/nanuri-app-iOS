309309907302a980e3b92a0f021672364b8285fb
Khyeji <khjji0502@gmail.com>
2022-07-21 16:40:18 +0900

    ADD: 마이페이지 설정 뷰 구현 완료

--- nanuri-app-renewal/Main/Tabbar/Setting/SettingViewController.swift
+++ nanuri-app-renewal/Main/Tabbar/Setting/SettingViewController.swift
@@ -8,28 +8,95 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    
+    private let tableView = UITableView(frame: .zero, style: .grouped)
+    private let sortOfSection: [[String]] = [["푸시 알림 설정"],
+                                        ["공지사항", "도움말", "문의하기"],
+                                        ["이용약관", "개인정보처리방침", "위치 기반 서비스 이용약관", "오픈소스 라이선스"],
+                                        ["로그아웃", "회원탈퇴"]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpView()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        let tabbarController = TabBarController()
-        tabbarController.modalTransitionStyle = .crossDissolve
-        tabbarController.modalPresentationStyle = .fullScreen
-        self.present(tabbarController, animated: false, completion: nil)
+    private func setUpView() {
+        self.navigationItem.title = "설정"
+        self.navigationController?.navigationBar.shadowImage = nil
+        let backButton = UIBarButtonItem(image: UIImage(named: "back_ic"), style: .plain, target: self, action: #selector(selectBackButton))
+        self.navigationItem.setLeftBarButton(backButton, animated: true)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        self.view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    @objc func selectBackButton() {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension SettingViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    // DataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sortOfSection.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return sortOfSection[0].count
+        } else if section == 1 {
+            return sortOfSection[1].count
+        } else if section == 2 {
+            return sortOfSection[2].count
+        }
+        return sortOfSection[3].count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "cell")
+        cell.selectionStyle = .none
+        
+        if indexPath.section == 0 {
+            let pushSwitch = UISwitch()
+            cell.contentView.addSubview(pushSwitch)
+            pushSwitch.snp.makeConstraints { make in
+                make.centerY.equalToSuperview()
+                make.right.equalTo(-16)
+            }
+            cell.textLabel?.text = sortOfSection[indexPath.section][indexPath.row]
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = sortOfSection[indexPath.section][indexPath.row]
+        } else if indexPath.section == 2 {
+            cell.textLabel?.text = sortOfSection[indexPath.section][indexPath.row]
+        } else {
+            if indexPath.row == 0 {
+                cell.textLabel?.text = sortOfSection[indexPath.section][0]
+            } else {
+                cell.textLabel?.text = sortOfSection[indexPath.section][1]
+                cell.textLabel?.textColor = .nanuriGray4
+                cell.backgroundColor = .clear
+            }
+        }
+        return cell
+    }
+    
+    // Delegate
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return .leastNormalMagnitude
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 48
+    }
 }
