4b2ce90f889aca675839db3001856c1d49a8a894
Khyeji <khjji0502@gmail.com>
2022-08-15 22:16:45 +0900

    ADD: 알림 뷰 그룹 및 뷰컨트롤러 생성

---
+++ nanuri-app-renewal/Main/Tabbar/Common/Notification/NotificationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NotificationViewController.swift
+//  nanuri-app-renewal
+//
+//  Created by heyji on 2022/08/15.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
