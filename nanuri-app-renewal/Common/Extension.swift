547e942a686086cfc65fd05e5a85b5e194c58622
min <75626237+mingging@users.noreply.github.com>
2022-08-24 00:35:50 +0900

    ADD: 카테고리 아이콘, 홈 슬라이드 페이저 추가

--- nanuri-app-renewal/Common/Extension.swift
+++ nanuri-app-renewal/Common/Extension.swift
@@ -331,3 +331,26 @@ extension UIImageView {
         self.sd_setImage(with: URL(string: url), placeholderImage: UIImage(named: "placeholder.png"))
     }
 }
+
+extension UIButton {
+    func alignTextBelow(spacing: CGFloat = 7.0) {
+        guard let image = self.imageView?.image else {
+            return
+        }
+
+        guard let titleLabel = self.titleLabel else {
+            return
+        }
+
+        guard let titleText = titleLabel.text else {
+            return
+        }
+
+        let titleSize = titleText.size(withAttributes: [
+            NSAttributedString.Key.font: titleLabel.font as Any
+        ])
+
+        titleEdgeInsets = UIEdgeInsets(top: spacing, left: -image.size.width, bottom: -image.size.height, right: 0)
+        imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0, bottom: 0, right: -titleSize.width)
+    }
+}
